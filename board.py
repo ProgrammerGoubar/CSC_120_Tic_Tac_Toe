7cd7a6c534abdc2942deea73ee5edc89452e5cc2
Abderrahim Goubar <ahmed.goubar05@gmail.com>
2022-07-17 12:32:34 -0400

    Added logic to the board by judging winners, losers, and a tie for the tic-tac-toe game; game's now complete in this current state

--- board.py
+++ board.py
@@ -1,15 +1,16 @@
-ticboard = [['-','-','-'],
-          ['-','-','-'],
-          ['-','-','-']]
+ticboard = ['-','-','-',
+          '-','-','-',
+          '-','-','-']
           
 game_still_occuring = True
 winner = None 
 
-current_player = 'x' 
+player_cur = 'x' 
 
 def print_board():
- for row in ticboard:
- print(row)
+ print(board[0] + board[1] + board[2])
+ print(board[3] + board[4] + board[5])
+ print(board[6] + board[7] + board[8])
    
 def play_the_game():
     print_board()
@@ -26,10 +27,23 @@ def play_the_game():
         
 
 def turn(player):
+    print("it's " + player "'s turn"
     pose = input("choose a position (pick only between 1-9)")
-    pose = int(pose)-1
     
-    ticboard[pose]= 'x'
+    valid = False
+    while not valid:
+    
+     while position not in ["1","2","3","4","5","6","7","8","9"]
+      pose = input("Choose a position from 1-9")
+      
+     pose = int(pose)-1
+    
+     if ticboard[pose]= '-':
+      valid = True
+     else:
+      print("You can't go there, try another")
+     
+    ticboard[pose] = player
     
     print_board()
 def check_over():
@@ -43,17 +57,31 @@ def check_win():
      column_win = check_columns()
      diagonal_win = check_diagonals():
      if row_winner():
-      winner = row_win()
+      winner = row_win
      elif column_winner:
-      winner = column_win()
+      winner = column_win
      elif diagonal_winner:
-      winner = diagonal_win()
+      winner = diagonal_win
+     else:
+      winner = None
      return
-     
+def flip():
+        global player_cur
+        if player_cur == "x":
+         player_cur = "o"
+        elif player_cur == "o":
+         player_cur = "x"
+        
 def check_tie():
-
-    return
+    global game_still_occuring
+    
+    if "-" not in board:
+        game_still_occuring=False
+        return True
+    else:
+     return False 
 def check_rows():
+    global game_still_occuring
     row_1 = ticboard[0] == ticboard[1] == ticboard[2] != '-'
     row_2 = ticboard[3] == ticboard[4] == ticboard[5] != '-'
     row_3 = ticboard[6] == ticboard[7] == ticboard[8] != '-'
@@ -65,11 +93,36 @@ def check_rows():
      return board[3]
     elif row_3:
      return board[6]
-    return
+    else 
+     return None
 def check_columns():
-    return
+
+    global game_still_occuring
+    column_1 = ticboard[0] == ticboard[3] == ticboard[6] != '-'
+    column_2 = ticboard[1] == ticboard[4] == ticboard[7] != '-'
+    column_3 = ticboard[2] == ticboard[5] == ticboard[8] != '-'
+    if column_1 or column_2 or column_3:
+        game_still_occuring = False
+    if column_1:
+     return board[0]
+    elif column_2:
+     return board[1]
+    elif column_3:
+     return board[2]
+    else:
+     return None
 def check_diagonals():
-    return
+    global game_still_occuring
+    diagonal_1 = ticboard[0] == ticboard[4] == ticboard[8] != '-'
+    diagonal_2 = ticboard[2] == ticboard[4] == ticboard[6] != '-'
+    if diagonal_1 or diagonal_2:
+      game_still_occuring = False
+    if diagonal_1:
+     return board[0]
+    elif diagonal_2:
+     return board[2]
+    else:
+     return None
 play_the_game()
     
     
